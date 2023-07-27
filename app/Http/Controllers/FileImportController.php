ce171ffa68e4cdf781b3a25cfca1e90792ca7edc
Ferl Jhon D. Javier <80177329+ferlerrorr@users.noreply.github.com>
2023-07-27 10:01:01 +0800

    Filltered

--- app/Http/Controllers/FileImportController.php
+++ app/Http/Controllers/FileImportController.php
@@ -45,39 +45,6 @@ class FileImportController extends Controller
         $flattenedData = array_map('intval', $flattenedData);
 
 
-        // $data_array = [
-        //     'H_InvNo' => $flattenedData['H_InvNo'],
-        //     'H_InvDate' => $flattenedData['H_InvDate'],
-        //     'H_InvAmt' => $flattenedData['H_InvAmt'],
-        //     'H_DiscAmt' => $flattenedData['H_DiscAmt'],
-        //     'H_StkFlag' => $flattenedData['H_StkFlag'],
-        //     'H_VendorID' => $flattenedData['H_VendorID'],
-        //     'H_VendorName' => $flattenedData['H_VendorName'],
-        //     'H_PORef' => $flattenedData['H_PORef'],
-        //     'H_SupCode' => $flattenedData['H_SupCode'],
-        //     'D_InvNo' => $flattenedData['D_InvNo'],
-        //     'D_ItemCode' => $flattenedData['D_Itemcode'],
-        //     'D_ItemName' => $flattenedData['D_ItemName'],
-        //     'D_ConvFact2' => $flattenedData['D_ConvFact2'],
-        //     'D_UOM' => $flattenedData['D_UOM'],
-        //     'D_UnitCost' => $flattenedData['D_UnitCost'],
-        //     'D_QtyShip' => $flattenedData['D_QtyShip'],
-        //     'D_QtyFree' => $flattenedData['D_QtyFree'],
-        //     'D_GrossAmt' => $flattenedData['D_GrossAmt'],
-        //     'D_PldAmt' => $flattenedData['D_PldAmt'],
-        //     'D_NetAmt' => $flattenedData['D_NetAmt'],
-        //     'D_SupCode' => $flattenedData['D_SupCode'],
-        //     'D_prefix' => $flattenedData['D_Prefix'],
-        //     'L_InvNo' => $flattenedData['L_InvNo'],
-        //     'L_ItemCode' => $flattenedData['L_ItemCode'],
-        //     'L_LotNo' => $flattenedData['L_LotNo'],
-        //     'L_ExpiryDD' => $flattenedData['L_ExpiryDD'],
-        //     'L_ExpiryMM' => $flattenedData['L_ExpiryMM'],
-        //     'L_ExpiryYYYY' => $flattenedData['L_ExpiryYYYY'],
-        //     'L_Qty' => $flattenedData['L_Qty'],
-        //     'L_SupCode' => $flattenedData['L_SupCode'],
-        // ];
-
         if ($_FILES["import_excel"]["name"] != '') {
             $allowed_extension = array('xls', 'csv', 'xlsx', 'txt', 'CSV');
             $file_array = explode(".", $_FILES["import_excel"]["name"]);
@@ -119,79 +86,9 @@ class FileImportController extends Controller
 
 
 
-                    // $data_array = [
-                    //     'H_InvNo' => $flattenedData['H_InvNo'],
-                    //     'H_InvDate' => $flattenedData['H_InvDate'],
-                    //     'H_InvAmt' => $flattenedData['H_InvAmt'],
-                    //     'H_DiscAmt' => $flattenedData['H_DiscAmt'],
-                    //     'H_StkFlag' => $flattenedData['H_StkFlag'],
-                    //     'H_VendorID' => $flattenedData['H_VendorID'],
-                    //     'H_VendorName' => $flattenedData['H_VendorName'],
-                    //     'H_PORef' => $flattenedData['H_PORef'],
-                    //     'H_SupCode' => $flattenedData['H_SupCode'],
-                    //     'D_InvNo' => $flattenedData['D_InvNo'],
-                    //     'D_ItemCode' => $flattenedData['D_Itemcode'],
-                    //     'D_ItemName' => $flattenedData['D_ItemName'],
-                    //     'D_ConvFact2' => $flattenedData['D_ConvFact2'],
-                    //     'D_UOM' => $flattenedData['D_UOM'],
-                    //     'D_UnitCost' => $flattenedData['D_UnitCost'],
-                    //     'D_QtyShip' => $flattenedData['D_QtyShip'],
-                    //     'D_QtyFree' => $flattenedData['D_QtyFree'],
-                    //     'D_GrossAmt' => $flattenedData['D_GrossAmt'],
-                    //     'D_PldAmt' => $flattenedData['D_PldAmt'],
-                    //     'D_NetAmt' => $flattenedData['D_NetAmt'],
-                    //     'D_SupCode' => $flattenedData['D_SupCode'],
-                    //     'D_prefix' => $flattenedData['D_Prefix'],
-                    //     'L_InvNo' => $flattenedData['L_InvNo'],
-                    //     'L_ItemCode' => $flattenedData['L_ItemCode'],
-                    //     'L_LotNo' => $flattenedData['L_LotNo'],
-                    //     'L_ExpiryDD' => $flattenedData['L_ExpiryDD'],
-                    //     'L_ExpiryMM' => $flattenedData['L_ExpiryMM'],
-                    //     'L_ExpiryYYYY' => $flattenedData['L_ExpiryYYYY'],
-                    //     'L_Qty' => $flattenedData['L_Qty'],
-                    //     'L_SupCode' => $flattenedData['L_SupCode'],
-                    // ];
 
-
-
-                    // $preserve_keys = ["H_InvNo", "D_InvNo", "L_InvNo", "H_Code", "D_Code", "L_Code"];
-                    // // Loop through each item (sub-array)
-                    // foreach ($excel_data as $item) {
-
-
-                    //     // Filter out elements with value 0, except for the preserved keys
-                    //     $import = array_filter($data_array, function ($value, $key) use ($item, $preserve_keys) {
-                    //         return ($value !== 0 || in_array($key, $preserve_keys)) && isset($item[$value]);
-                    //     }, ARRAY_FILTER_USE_BOTH);
-                    //     $result[] = array_values($import); // Re-index the array and add to the result
-                    // }
-
-
-                    // foreach ($data_array as $key => $value) {
-                    //     if ($value === 0 && !in_array($key, $preserve_keys)) {
-                    //         unset($data_array[$key]);
-                    //     }
-                    // }
-
-
-                    // $result = [];
-
-                    // // Loop through each item (sub-array)
-                    // foreach ($excel_data as $item) {
-
-
-
-                    //     $import = []; // Initialize the $import array
-
-                    //     // Loop through each index name in $data_array
-                    //     foreach ($data_array as $index => $index_name) {
-                    //         // Append the index (key) to the $import array instead of the value
-                    //         $import[$index] = $item[$data_array[$index]];
-                    //     }
-                    //     $result[] =  $import;
-                    // }
-
-
+                    $result = [];
+                    $H = [];
 
                     foreach ($excel_data as $item) {
                         $firstItemValue = $item[0];
@@ -205,16 +102,85 @@ class FileImportController extends Controller
                                 break;
                             case 'L':
                                 $L[] = $item;
+                                $l_data_array = [
+                                    'L_InvNo' => $item[1],
+                                    'L_ItemCode' => $item[3],
+                                    'L_LotNo' => $item[4],
+                                    'L_ExpiryDD' => $item[5],
+                                    'L_ExpiryMM' => $item[5],
+                                    'L_ExpiryYYYY' => $item[6],
+                                    'L_Qty' => $item[7]
+                                ];
+
                                 break;
-                                // If the first item doesn't match any case, you can handle it as needed.
-                                // For example, skip it or add it to a separate array for unknown cases.
                         }
+                    }
+
+                    // Iterate through each item in the array $H
+                    foreach ($H as $item) {
+                        // Extract required elements from the current item in $H
+                        $H_InvNo = $item[1];
+                        $H_PORef = $item[8];
+
+                        // Create a new associative array with the extracted values
+                        $filteredItem = array(
+                            'H_InvNo' => $H_InvNo,
+                            'H_PORef' => $H_PORef
+                        );
+
+                        // // Add the filteredItem array to the $result
+                        // $result[] = $filteredItem;
+                    }
+
+                    // Iterate through each item in the array $D
+                    foreach ($D as $item) {
+                        // Extract required elements from the current item in $D
+                        $D_InvNo = $item[1];
+                        $D_ItemCode = $item[3];
+
+                        // Create a new associative array with the extracted values
+                        $filteredItem = array(
+                            'D_InvNo' => $D_InvNo,
+                            'D_ItemCode' => $D_ItemCode
+                        );
+
+                        // Add the filteredItem array to the $result
+                        // $result[] = $filteredItem;
+                    }
+
+                    // Iterate through each item in the array $L
+                    foreach ($L as $item) {
+                        // Extract required elements from the current item in $L
+                        $L_InvNo = $item[1];
+                        $L_ItemCode = $item[3];
+                        $L_LotNo = $item[4];
+                        $L_ExpiryDD = $item[5];
+                        $L_ExpiryMM = $item[5];
+                        $L_ExpiryYYYY = $item[6];
+                        $L_Qty = $item[7];
+
+
+                        // Create a new associative array with the extracted values
+                        $filteredItem = array(
+                            'L_InvNo' => $L_InvNo,
+                            'L_ItemCode' =>  $L_ItemCode,
+                            'L_LotNo' => $L_LotNo,
+                            'L_ExpiryDD' => $L_ExpiryDD,
+                            'L_ExpiryMM' => $L_ExpiryMM,
+                            'L_ExpiryYYYY' => $L_ExpiryYYYY,
+                            'L_Qty' =>   $L_Qty
+                        );
+
+                        // Add the filteredItem array to the $result
+                        $result[] = $filteredItem;
                     }
 
 
 
 
-                    return response()->json($H, 200);
+
+
+                    return response()->json($result, 200);
                 } else {
 
 
